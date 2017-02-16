3f89a53817e28aa6e65455bc68e003cd23962fdf
rougle <ossi.puustinen92@gmail.com>
2017-02-16 13:15:23 +0200

    Added procfile

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const token = '337263517:AAG-MGtqZY_3MEU4lcRDvVuyf28e3AF-YdU';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\hai/, (message) => {
-	bot.sendMessage(message.chat.id, 'oh hai!');
+	bot.sendMessage(message.chat.id, 'oh hai');
 });
 
 console.log('It lives!')
