d9388cc1f3d0b3ce5da64035cddddc84d12b357f
rougle <ossi.puustinen92@gmail.com>
2017-02-16 13:27:40 +0200

    now runs on express

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
+const express = require ('express');
 const TelegramBot = require('node-telegram-bot-api');
+const packageInfo = require('./package.json')
 
 const token = '337263517:AAG-MGtqZY_3MEU4lcRDvVuyf28e3AF-YdU';
 const bot = new TelegramBot(token, { polling: true });
@@ -7,4 +9,8 @@ bot.onText(/\hai/, (message) => {
 	bot.sendMessage(message.chat.id, 'oh hai');
 });
 
-console.log('It lives!')
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ version: packageInfo.version }));
+app.listen(process.env.PORT || 3000, () => console.log('It lives! Again!'));
