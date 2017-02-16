a38ac4326f205497b091689649b03ebe36f91ff3
rougle <ossi.puustinen92@gmail.com>
2017-02-16 14:42:26 +0200

    Chuchbot

--- bot.js
+++ bot.js
@@ -1,16 +1,34 @@
-const express = require ('express');
+const express = require('express');
+const request = require('request'); 
 const TelegramBot = require('node-telegram-bot-api');
 const packageInfo = require('./package.json')
 
 const token = '337263517:AAG-MGtqZY_3MEU4lcRDvVuyf28e3AF-YdU';
 const bot = new TelegramBot(token, { polling: true });
 
+const app = express();
+
 bot.onText(/\hai/, (message) => {
 	bot.sendMessage(message.chat.id, 'oh hai');
 });
 
 
-const app = express();
+const url = 'https://api.chucknorris.io/jokes/random';
+var jsonbody = '';
+
+bot.onText(/\chuck/, (message) => {
+	request(url, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	      jsonbody = JSON.parse(body)
+	      console.log(jsonbody.value);
+	    }
+	})
+	bot.sendMessage(message.chat.id, jsonbody.value);
+});
+
+
 
 app.get('/', (req, res) => res.json({ version: packageInfo.version }));
+
+
 app.listen(process.env.PORT || 3000, () => console.log('It lives! Again!'));
